1ba7e011eb78093cd08cd7201c73b39a360b1182
charskiy <mtovkay@gmail.com>
2024-07-09 21:40:53 +0200

    initial

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+
+namespace SpeechToTextConsoleApp
+{
+    class Program
+    {
+        private static readonly string subscriptionKey = "3d703747154e43009fa03143a4cf1cc9";
+        private static readonly string region = "northeurope";
+
+        static async Task Main(string[] args)
+        {
+            var config = SpeechConfig.FromSubscription(subscriptionKey, region);
+
+            using (var recognizer = new SpeechRecognizer(config))
+            {
+                Console.WriteLine("Say something...");
+
+                var result = await recognizer.RecognizeOnceAsync();
+
+                if (result.Reason == ResultReason.RecognizedSpeech)
+                {
+                    Console.WriteLine($"Recognized: {result.Text}");
+                }
+                else if (result.Reason == ResultReason.NoMatch)
+                {
+                    Console.WriteLine("No speech could be recognized.");
+                }
+                else if (result.Reason == ResultReason.Canceled)
+                {
+                    var cancellation = CancellationDetails.FromResult(result);
+                    Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+
+                    if (cancellation.Reason == CancellationReason.Error)
+                    {
+                        Console.WriteLine($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
+                    }
+                }
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
